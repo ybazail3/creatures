9a5527c7e4fc04158d55181f2266ac500408ef38
yazzy14 <yazminbazail@icloud.com>
2023-11-28 12:37:05 -0800

    re-formatted the code

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 let homes = document.querySelectorAll("[data-creature-home]");
-      let types = document.querySelectorAll("[data-creature-type]");
+let types = document.querySelectorAll("[data-creature-type]");
 
-      function showHome() {
-        homes.forEach((home) => {
-          if (home.dataset.creatureHome === "land") {
-            home.style.backgroundColor = "green";
-          } else if (home.dataset.creatureHome === "air") {
-            home.style.backgroundColor = "yellow";
-          } else {
-            home.style.backgroundColor = "blue";
-          }
-        });
+function showHome() {
+  homes.forEach((home) => {
+    if (home.dataset.creatureHome === "land") {
+      home.style.backgroundColor = "green";
+    } else if (home.dataset.creatureHome === "air") {
+      home.style.backgroundColor = "yellow";
+    } else {
+      home.style.backgroundColor = "blue";
+    }
+  });
 
-        // alert("showHome()");
-      } //end showHome()
+  // alert("showHome()");
+} //end showHome()
 
-      function showType() {
-        types.forEach((type) => {
-          if (type.dataset.creatureType === "animal") {
-            type.style.borderColor = "red";
-          } else {
-            type.style.border = "orange dashed";
-          }
-        });
+function showType() {
+  types.forEach((type) => {
+    if (type.dataset.creatureType === "animal") {
+      type.style.borderColor = "red";
+    } else {
+      type.style.border = "orange dashed";
+    }
+  });
 
-        // alert("showType()");
-      } //end showType()
+  // alert("showType()");
+} //end showType()
 
-      function clearAll() {
-        homes.forEach((home) => {
-          home.style = "";
-        });
-        types.forEach((type) => {
-          type.style = "";
-        });
-        // alert("clearAll()");
-      } //end clearAll()
+function clearAll() {
+  homes.forEach((home) => {
+    home.style = "";
+  });
+  types.forEach((type) => {
+    type.style = "";
+  });
+  // alert("clearAll()");
+} //end clearAll()
